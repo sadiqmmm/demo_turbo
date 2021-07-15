f1791d1cb5343685279a73eaa963b866758bf778
Mohammed Sadiq <sadiqmmm@gmail.com>
2021-07-15 19:11:30 +0530

    Add the comment and turbo_stream

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
+
   belongs_to :project
+  after_create_commit { broadcast_prepend_to [project, :comments], target: "#{dom_id(project)}_comments" }
 end
