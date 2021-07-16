b59fa6425a04b78686a552d94e5fe67d2a9451d7
Mohammed Sadiq <sadiqmmm@gmail.com>
2021-07-17 02:34:55 +0530

    Add more boardcasts method to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
-  after_create_commit { broadcast_prepend_to target: 'projects' }
   has_many :comments, dependent: :destroy
+
+  after_create_commit { broadcast_prepend_to 'projects' }
+  after_update_commit { broadcast_replace_to 'projects' }
+  after_destroy_commit { broadcast_remove_to 'projects' }
 end
